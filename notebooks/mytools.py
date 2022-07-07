e46a539eb16288cf8628df41fd9a3b97b1540c43
Gaston Bujia <gastonbujia@gmail.com>
2022-07-07 13:25:12 -0300

    actualizacion en responses

--- notebooks/mytools.py
+++ notebooks/mytools.py
@@ -1,5 +1,7 @@
+from distutils import dist
 import os
 import json
+from turtle import screensize
 import numpy as np
 import pandas as pd
 from scripts.loader import load_dict_from_json
@@ -22,13 +24,18 @@ def distance_to_target(trial):
     x_target = (trial['target_bbox'][2] - trial['target_bbox'][0]) / 2 + trial['target_bbox'][0]
     y_target = (trial['target_bbox'][3] - trial['target_bbox'][1]) / 2 + trial['target_bbox'][1]
     try:
-        return float(np.sqrt((x_target - trial['response_X'])**2 + (y_target - trial['response_Y'])**2 ))
+        #return float(np.sqrt((x_target - trial['response_X'])**2 + (y_target - trial['response_Y'])**2 ))
+        return np.linalg.norm(np.array([x_target, y_target]) - np.array([trial['response_X'], trial['response_Y']]))
     except TypeError:
         print(trial['X'])
         print(trial['response_X'])
         print(type(trial['response_X']))
         print(x_target)
         print(type(x_target))
+
+def target_found_response(target_box, response, response_size):
+    x1, y1, x2, y2 = target_box
+    return None
         
 def add_responses(scanpaths_path, responses_path, calculate_features=True):
     responses = pd.read_csv(os.path.join(responses_path, 'responses_data.csv')).set_index(['subj_id','image'])
@@ -41,8 +48,16 @@ def add_responses(scanpaths_path, responses_path, calculate_features=True):
             subject_scanpaths = load_dict_from_json(scanpaths_file)
             for img, val in subject_scanpaths.items():
                 val['subject_name']    = responses.loc[subj_id, img]['subj']
-                val['response_X']      = float(responses.loc[subj_id, img]['response_x'])
-                val['response_Y']      = float(responses.loc[subj_id, img]['response_y'])
+                # necesito corregir por el tamaño de la pantalla
+                # DUDA: el target bbox esta en tamaño pantalla?
+                screen_height, screen_width = float(val['screen_height']), float(val['screen_width'])
+                image_height, image_width   = float(val['image_height']), float(val['image_width'])
+                offset_height, offset_width = (screen_height - image_height)/2, (screen_width-image_width)/2
+                val['response_X']           = float(responses.loc[subj_id, img]['response_x']) - offset_width
+                val['response_Y']           = float(responses.loc[subj_id, img]['response_y']) - offset_height
+                # previo a la correccion por offset
+                #val['response_X']      = float(responses.loc[subj_id, img]['response_x'])
+                #val['response_Y']      = float(responses.loc[subj_id, img]['response_y'])
                 val['response_size']   = responses.loc[subj_id, img]['response_size']
                 val['response_click']  = responses.loc[subj_id, img]['response_time_click']
                 val['response_circle'] = responses.loc[subj_id, img]['response_time_circle']
@@ -70,8 +85,10 @@ def get_responses_features(subjs):
                     'target_found_response': data['target_found_response'],
                     'response_size': data['response_size'],
                     'distance_to_target': data['distance_to_target'],
-                    'delta_time_response': data['delta_time_response']
+                    'delta_time_response': data['delta_time_response'],
+                    'response_x': data['response_X'],
+                    'response_y': data['response_Y'], 
                     }
             df.append(upd)
-    return df
+    return pd.DataFrame(df)
     
---
+++ notebooks/responses_notas.md
@@ -0,0 +1,19 @@
+# Dudas
+
+- El bbox esta como w,h o h,w
+- La respuesta del sujeto estaba en referencia al tamaño de la pantalla?
+- La respuesta del sujeto la daban siempre?
+- El tamaño de la respuesta es en pixeles?
+
+# Notas
+
+- Parece que las fijaciones estan en el tamaño de la pantalla
+- Criterio para definir cuando la respuesta del sujeto efectivamente toca el target, probar opciones
+
+# TODO
+
+- Chequear las dimensiones de las scanpaths parece ser que **X:fila** e **Y:columna**
+- Fijarse que pasa con los sujetos que tiene respuesta que caen afuera de la imagen
+- Cambiar la decisión de 
+- Armar un codiguito para ver los sujetos que lo encontraron al target pero en la resupuesta parece que no
+- 
