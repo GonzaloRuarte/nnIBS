51fdc4be573229e4b962eff3afa3e6eada39deb0
Gaston Bujia <gastonbujia@gmail.com>
2022-08-11 13:30:22 -0300

    constantes para las notebooks

---
+++ notebooks/constants.py
@@ -0,0 +1,12 @@
+from os import path
+# Configuration constants
+# All paths are relative to root
+CONFIG_DIR    = 'configs'
+DATASETS_PATH = 'Datasets'
+RESULTS_PATH  = 'Results'
+MODELS_PATH = 'Models'
+SALIENCY_PATH = path.join('data', 'saliency')
+TARGET_SIMILARITY_PATH = path.join('data', 'target_similarity_maps')
+
+SIGMA      = [[4000, 0], [0, 2600]]
+IMAGE_SIZE = (768, 1024)
