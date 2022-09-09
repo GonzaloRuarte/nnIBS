f0cd80ef2ca408febe36b87d58a1330ace8429f7
GonzaloRuarte <gonzalorpg@gmail.com>
2022-09-09 19:27:48 -0300

    resnet 18

--- visualsearch/gng_model/go_no_go.py
+++ visualsearch/gng_model/go_no_go.py
@@ -7,13 +7,13 @@ class Net(models.ResNet):
     def __init__(self, num_classes=1000, **kwargs):
         # Start with standard resnet152 defined here
 
-        super().__init__(block = models.resnet.Bottleneck, layers = [3, 8, 36, 3], num_classes = 1000, **kwargs)
+        super().__init__(block = models.resnet.BasicBlock, layers=[2, 2, 2, 2], num_classes = 1000, **kwargs)
         #black and white images
 
         self.inplanes = 64
         self.conv1 = nn.Conv2d(1, self.inplanes, kernel_size=7, stride=2, padding=3, bias=False)
         #pretrained resnet-152 by default
-        state_dict = load_state_dict_from_url("https://download.pytorch.org/models/resnet152-394f9c45.pth")
+        state_dict = load_state_dict_from_url("https://download.pytorch.org/models/resnet18-f37072fd.pth")
         #to make it work in grayscale images
         conv1_weight = state_dict['conv1.weight']
         state_dict['conv1.weight'] = conv1_weight.sum(dim=1, keepdim=True)
